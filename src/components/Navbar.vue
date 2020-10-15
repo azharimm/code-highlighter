88f449ff8224f2f8b76adea558e2257a646c0fdf
Azhari Muhammad M <azhari.marzan@gmail.com>
2020-10-15 13:11:38 +0700

    + navbar component

---
+++ src/components/Navbar.vue
@@ -0,0 +1,39 @@
+<template>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">
+            <img src="https://cdn4.iconfinder.com/data/icons/simple-lines-part-1/32/Code_Quote_Frontend_Programming_Syntax_Slash-256.png" width="30" height="30" class="d-inline-block align-top" alt="" loading="lazy">
+            Highlighter
+        </a>
+        <button
+            class="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+        >
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">
+                        Login
+                    </a>
+                </li>
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">
+                        Register
+                    </a>
+                </li>
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">
+                        Azhari
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</template>
