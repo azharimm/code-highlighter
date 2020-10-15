d5efd2ce85ea0de5e4e04bbbf08aa81cb812031e
Azhari Muhammad M <azhari.marzan@gmail.com>
2020-10-15 15:00:09 +0700

    + base component layout

---
+++ src/components/Cols.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="col-12">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,3 +1,49 @@
 <template>
-    <h1>Home Page</h1>
+	<Container class="mt-5">
+		<Row>
+            <Cols class="col-md-4">
+                <div class="card">
+                    <div class="card-body">
+                        This is the card
+                    </div>
+                </div>
+            </Cols>
+            <Cols class="col-md-8">
+            <div class="card">
+                <div class="card-body">
+                    This is the card
+                </div>
+            </div>
+            </Cols>
+        </Row>
+        <Row class="mt-3">
+            <Cols class="col-md-4">
+                <div class="card">
+                    <div class="card-body">
+                        This is the card
+                    </div>
+                </div>
+            </Cols>
+            <Cols class="col-md-8">
+            <div class="card">
+                <div class="card-body">
+                    This is the card
+                </div>
+            </div>
+            </Cols>
+        </Row>
+	</Container>
 </template>
+
+<script>
+import Container from "../components/Container";
+import Row from "../components/Row";
+import Cols from "../components/Cols";
+export default {
+	components: {
+		Container,
+		Row,
+		Cols,
+	},
+};
+</script>
